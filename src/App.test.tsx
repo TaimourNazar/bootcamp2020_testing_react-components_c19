d6c7f5441879554e350f27af7507343175befcc5
Taimour <taimour.nazar1@gmail.com>
2020-08-05 15:10:04 +0500

    v1

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,20 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('check react test', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/react test/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('check react test component', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/react test component/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('check and save to reload', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/and save to reload/i);
   expect(linkElement).toBeInTheDocument();
 });
